05a9806486158d5f723c99c1762f13cf8397ffac
Henry Corredor González <henry.corredor7570@gmail.com>
2024-01-12 15:51:40 -0500

    cambios

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:3000/api",
+    // baseURL: "http://localhost:3000/api",
+    baseURL: "https://taskharbor-production.up.railway.app",
     withCredentials: true,
 })
 
